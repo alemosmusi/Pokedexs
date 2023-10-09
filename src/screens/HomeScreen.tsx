89ba40328aab8951d35281f0bceb85dd7841decd
Agu <alemosmusi@gmail.com>
2023-10-09 20:47:58 -0300

    agregado usepokemon

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Image, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
 
 export const HomeScreen = () => {
 
   const {top}=useSafeAreaInsets()
+
+  usePokemonPaginated()
 
 
   return (
