bd01462cd211d8a078052864b3c1edb631b1cf07
Agu <alemosmusi@gmail.com>
2023-10-09 21:29:47 -0300

    agregado flat list pokemon

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from "../api/pokemonApi";
-import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/pokemonIterfaces";
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonIterfaces';
 
 
 export const usePokemonPaginated = () => {
+
+    const [isLoading, setIsLoading] = useState(true)
 
     const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
   
@@ -11,6 +13,7 @@ export const usePokemonPaginated = () => {
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
 
     const loadPokemons = async ()=>{
+        setIsLoading(true)
         const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current)
         nextPageUrl.current = resp.data.next
         mapPokemonList(resp.data.results)
@@ -18,7 +21,22 @@ export const usePokemonPaginated = () => {
 
     const mapPokemonList = ( pokemonList: Result[]) => {
 
-        pokemonList.forEach(poke => console.log(poke.name))
+
+
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({name, url})=>{
+
+            const urlParts = url.split('/');
+            const id = urlParts[urlParts.length - 2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+            return {
+                id,
+                picture,
+                name
+            }
+        })
+
+        setSimplePokemonList([...simplePokemonList,...newPokemonList])
+        setIsLoading(false)
     }
     
     useEffect(() => {
@@ -26,7 +44,9 @@ export const usePokemonPaginated = () => {
     }, [])
 
     return {
-        simplePokemonList
+        isLoading,
+        simplePokemonList,
+        loadPokemons
     }
     
     
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
@@ -8,7 +8,9 @@ export const HomeScreen = () => {
 
   const {top}=useSafeAreaInsets()
 
-  usePokemonPaginated()
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated()
+
+
 
 
   return (
@@ -17,11 +19,43 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-        <Text style={{
+
+
+      <FlatList 
+      
+        data={simplePokemonList}
+        keyExtractor={(pokemon)=>pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item,index})=> (
+          <Image 
+            source={{uri: item.picture}}
+            style={{
+              width:100,
+              height:100
+            }}
+          />
+        )}
+
+        //infinite scroll
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+
+        ListFooterComponent={(
+              <ActivityIndicator 
+                style={{height: 100}}
+                size={20}
+                color="grey"
+              />
+              )}
+      
+      />
+
+
+        {/* <Text style={{
             ...styles.title,
             ...styles.globalMargin,
             top: top + 20
-            }}>Pokedex</Text>
+            }}>Pokedex</Text> */}
     </>
   )
 }
